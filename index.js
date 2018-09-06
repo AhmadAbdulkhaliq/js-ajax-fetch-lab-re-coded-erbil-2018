4572c122f9c9944054731bf3add623cb223f7279
ahmad Abdulkhaliq Osman <ahmadabdulkhaliq384@gmail.com>
2018-09-06 03:15:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,22 @@ const fork = `${userName}/javascript-fetch-lab`
 
 
 
+//Fetch all issues through the Github API and display / append to the DOM
+
+function getIssues(data) {
+  fetch(`${baseApi}repos/${fork}/issues`).
+    then(resp => {
+      resp.json().then( data => {
+        for (let i = 0; i < data.length; i++){
+          displayIssue(new Issue(data[i]));
+        }
+      } )
+    })
+}
+
+function displayIssue(issue) {
+  $('#issues').append(issue.template())
+}
 
 //Fetch and show repo info
 
